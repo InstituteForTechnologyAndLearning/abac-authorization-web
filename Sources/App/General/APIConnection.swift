8a24a2e15088f038cb617a30820d73709b2ce847
Carmi Weinzweig <carmi-github@jimiscool.com>
2023-12-15 09:52:59 -0800

    Eliminated the 8080 so that it only appears in only one place. Ensures that all code could run (previously, the outer "?? 8080" was decorative and satisfied the need for a non-optional Int). Not critical, just a preference.

--- Sources/App/General/APIConnection.swift
+++ Sources/App/General/APIConnection.swift
@@ -3,7 +3,7 @@ import Vapor
 enum APIConnection {
     static let scheme = "http"
     static let host = String(Environment.get("HTTP_HOST") ?? "localhost")
-    static let port = Int(Environment.get("HTTP_PORT") ?? "8080") ?? 8080
+    static let port = Int(Environment.get("HTTP_PORT") ?? "") ?? 8080
     
     static let url = "\(APIConnection.scheme)://\(APIConnection.host):\(APIConnection.port)"
     static let apiBaseURL = Environment.get("API_BASE_URL") ?? APIConnection.url
